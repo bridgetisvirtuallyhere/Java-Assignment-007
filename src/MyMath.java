8b3647c9926d01fa7372eb44c6101a85f58bd195
bridgetisvirtuallyhere <bridgetjacosta@gmail.com>
2022-10-13 11:16:35 -0700

    Bridget Acosta 10/13/2022 Morning

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
-
+/*
+*@author Bridget Acosta
+* 10/13/2022
+ */
 public class MyMath {
+    //Use the flowchart above to implement a method gcf in the MyMath class
+    // that calculates the greatest common factor of 2 numbers.
+    //Hint 1: This flowchart depicts a while loop and conditional
+    private final static Scanner s = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void gcf() {
+
+        System.out.println("Type a number:");
+        int a = Integer.parseInt(s.nextLine());
+        System.out.println("Type a number:");
+        int b = Integer.parseInt(s.nextLine());
+
+        while (b != 0) {
+            if (a > b) {
+                int c = b;
+                b = a % b;
+                a = c;
+                System.out.println("GCF is: " +c);
+            } else {
+                b = b % a;
+            }
+        }
     }
-}
+        public static void main (String[]args) {
+            gcf();
+        }
+    }
